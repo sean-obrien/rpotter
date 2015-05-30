84042572bd28e9a5316871bacb8f5c166cae99f0
sean-obrien <saobrien@gmail.com>
2015-05-30 01:53:43 -0500

    Tinkering - Mini Maker Faire edition.

--- rpotter.py
+++ rpotter.py
@@ -8,26 +8,42 @@ O O-O
   O
   
 Raspberry Potter
-Version 0.1
+Version 0.1.1
 
-Use your interactive Harry Potter wands to control the IoT.  
+Use your own wand or your interactive Harry Potter wands to control the IoT.  
 
 
-Copyright (c) 2015 Sean O'Brien
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
+Copyright (c) 2015 Sean O'Brien.  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 '''
+import io
 import cv2
+from cv2 import *
+from cv import *
+import picamera
 import numpy as np
 import threading
 import sys
 import math
+import time
+import pigpio
+
+GPIOS=32
+MODES=["INPUT", "OUTPUT", "ALT5", "ALT4", "ALT0", "ALT1", "ALT2", "ALT3"]
+
+pi = pigpio.pi()
+
+#pin for Powerswitch (Lumos,Nox)
+switch_pin = 16
+pi.set_mode(switch_pin,pigpio.OUTPUT)
+
+#pin for Trinket (Colovario)
+trinket_pin = 12
+pi.set_mode(trinket_pin,pigpio.OUTPUT)
 
 print "Initializing point tracking"
+
 
 # Parameters
 lk_params = dict( winSize  = (15,15),
@@ -38,33 +54,42 @@ dilation_params = (5, 5)
 movment_threshold = 80
 
 # start capturing
-cam = cv2.VideoCapture(0)
 cv2.namedWindow("Raspberry Potter")
+cam = cv2.VideoCapture(0)
+cam.set(3, 640)
+cam.set(4, 480)
+
 
 def Spell(spell):    
     #clear all checks
     ig = [[0] for x in range(15)] 
     #Invoke IoT (or any other) actions here
     cv2.putText(mask, spell, (5, 25),cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255,0,0))
+    if (spell=="Colovaria"):
+	print "GPIO trinket"
+	pi.write(trinket_pin,0)
+	time.sleep(1)
+	pi.write(trinket_pin,1)
+    elif (spell=="Lumos"):
+	print "GPIO ON"
+	pi.write(switch_pin,1)
+    elif (spell=="Nox"):
+	print "GPIO OFF"
+	pi.write(switch_pin,0)
     print "CAST: %s" %spell
+    
 
 def IsGesture(a,b,c,d,i):
     print "point: %s" % i
     #record basic movements - TODO: trained gestures
-    #for z in ig[i]:
-    #    print z
     if ((a<(c-5))&(abs(b-d)<1)):
-        ig[i].append("right")
-    #    print "right"
-    elif ((c<(a-5))&(abs(b-d)<1)):
         ig[i].append("left")
-    #    print "left"  
+    elif ((c<(a-5))&(abs(b-d)<1)):
+        ig[i].append("right")
     elif ((b<(d-5))&(abs(a-c)<5)):
         ig[i].append("up")
-    #    print "up" 
     elif ((d<(b-5))&(abs(a-c)<5)):
         ig[i].append("down")
-    #    print "down"       
     #check for gesture patterns in array
     astr = ''.join(map(str, ig[i]))
     if "rightup" in astr:
@@ -79,109 +104,104 @@ def FindWand():
     global rval,old_frame,old_gray,p0,mask,color,ig,img,frame
     try:  
         rval, old_frame = cam.read()
-        old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
-        
-        # adaptive histogram equalization
-        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(3,3))
-        old_gray = clahe.apply(old_gray)
-        old_gray = cv2.GaussianBlur(old_gray,blur_params,2,2);
-
-        # dilate
-        dilate_kernel = np.ones(dilation_params, np.uint8)
-        dilate_kernel = np.ones(dilation_params, np.uint8)
-        old_gray = cv2.dilate(old_gray, dilate_kernel, iterations=1)
+	cv2.flip(old_frame,1,old_frame)
+	old_gray = cvtColor(old_frame,cv2.COLOR_BGR2GRAY)
+        #equalizeHist(old_gray,old_gray)
+	#old_gray = GaussianBlur(old_gray,(9,9),1.5)
+        #dilate_kernel = np.ones(dilation_params, np.uint8)
+        #old_gray = cv2.dilate(old_gray, dilate_kernel, iterations=1)
 
         #TODO: trained image recognition
-        p0 = cv2.HoughCircles(old_gray,cv2.cv.CV_HOUGH_GRADIENT,3,100,param1=100,param2=30,minRadius=5,maxRadius=15)
-        p0.shape = (p0.shape[1], 1, p0.shape[2])
-        p0 = p0[:,:,0:2]  
+        p0 = cv2.HoughCircles(old_gray,cv2.cv.CV_HOUGH_GRADIENT,3,100,param1=100,param2=30,minRadius=4,maxRadius=15)
+	p0.shape = (p0.shape[1], 1, p0.shape[2])
+        p0 = p0[:,:,0:2] 
         mask = np.zeros_like(old_frame)
-        ig = [[0] for x in range(15)] 
+        ig = [[0] for x in range(20)] 
         print "finding..."
         threading.Timer(3, FindWand).start()
     except:
+        e = sys.exc_info()[1]
+        print "Error: %s" % e 
+	cam.release()  
         cv2.destroyAllWindows()
-        cam.release()  
         exit
         
 def TrackWand():
-    if cam.isOpened(): # try to get the first frame
-        # Take first frame and find circles in it
         global rval,old_frame,old_gray,p0,mask,color,ig,img,frame
         color = (0,0,255)
-        rval, old_frame = cam.read()
-        old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
-        # adaptive histogram equalization
-        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(3,3))
-        old_gray = clahe.apply(old_gray)
-        old_gray = cv2.GaussianBlur(old_gray,blur_params,2,2);
-
-        # dilate
-        dilate_kernel = np.ones(dilation_params, np.uint8)
-        old_gray = cv2.dilate(old_gray, dilate_kernel, iterations=1)
-        p0 = cv2.HoughCircles(old_gray,cv2.cv.CV_HOUGH_GRADIENT,3,100,param1=100,param2=30,minRadius=5,maxRadius=15)
-        p0.shape = (p0.shape[1], 1, p0.shape[2])
-        p0 = p0[:,:,0:2]    
-        # Create a mask image for drawing purposes
-        mask = np.zeros_like(old_frame)
-    else:
-        rval = False
-
-    while rval:
-        rval,frame = cam.read()
-        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        # adaptive histogram equalization
-        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(3,3))
-        frame_gray = clahe.apply(frame_gray)
-        frame_gray = cv2.GaussianBlur(frame_gray,blur_params,2,2);
-
-        # dilate
-        dilate_kernel = np.ones(dilation_params, np.uint8)
-        frame_gray = cv2.dilate(frame_gray, dilate_kernel, iterations=1)
-        
-        # calculate optical flow
-        p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
-
-        # Select good points
+	rval, old_frame = cam.read()
+	cv2.flip(old_frame,1,old_frame)
+	old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
+        #equalizeHist(old_gray,old_gray)
+	#old_gray = GaussianBlur(old_gray,(9,9),1.5)
+	#dilate_kernel = np.ones(dilation_params, np.uint8)
+        #old_gray = cv2.dilate(old_gray, dilate_kernel, iterations=1)
+        # Take first frame and find circles in it
+        p0 = cv2.HoughCircles(old_gray,cv2.cv.CV_HOUGH_GRADIENT,3,100,param1=100,param2=30,minRadius=4,maxRadius=15)
         try:
-            good_new = p1[st==1]
-            good_old = p0[st==1]
-
-            # draw the tracks
-            for i,(new,old) in enumerate(zip(good_new,good_old)):
-                a,b = new.ravel()
-                c,d = old.ravel()
-                # only try to detect gesture on highly-rated points (below 15)
-                if (i<15):
-                    IsGesture(a,b,c,d,i)
-                dist = math.hypot(a - c, b - d)
-                if (dist<movment_threshold):
-                    cv2.line(mask, (a,b),(c,d),(0,255,0), 2)
-                cv2.circle(frame,(a,b),5,color,-1)
-                cv2.putText(frame, str(i), (a,b), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0,0,255))
-        except IndexError:
-            print "Index error"           
+		p0.shape = (p0.shape[1], 1, p0.shape[2])
+        	p0 = p0[:,:,0:2] 
         except:
-            e = sys.exc_info()[0]
-            print "Error: %s" % e 
-        img = cv2.add(frame,mask)
+            	print "No points found"         
+	# Create a mask image for drawing purposes
+        mask = np.zeros_like(old_frame)
 
-        cv2.putText(img, "Press ESC to close.", (5, 25),
+	while True:
+		rval, frame = cam.read()
+		cv2.flip(frame,1,frame)
+        	frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        	#equalizeHist(frame_gray,frame_gray)
+		#frame_gray = GaussianBlur(frame_gray,(9,9),1.5)
+		#dilate_kernel = np.ones(dilation_params, np.uint8)
+        	#frame_gray = cv2.dilate(frame_gray, dilate_kernel, iterations=1)
+   		try:     
+        	# calculate optical flow
+			p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
+
+        	# Select good points
+            		good_new = p1[st==1]
+            		good_old = p0[st==1]
+
+            		# draw the tracks
+            		for i,(new,old) in enumerate(zip(good_new,good_old)):
+                		a,b = new.ravel()
+                		c,d = old.ravel()
+                		# only try to detect gesture on highly-rated points (below 15)
+                		if (i<15):
+                    			IsGesture(a,b,c,d,i)
+                		dist = math.hypot(a - c, b - d)
+                		if (dist<movment_threshold):
+                    			cv2.line(mask, (a,b),(c,d),(0,255,0), 2)
+                		cv2.circle(frame,(a,b),5,color,-1)
+                		cv2.putText(frame, str(i), (a,b), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0,0,255))
+        	except IndexError:
+            		print "Index error"  
+		        cam.release()
+		        cv2.destroyAllWindows()
+        		break          
+        	except:
+            		e = sys.exc_info()[0]
+            		print "Error: %s" % e 
+			cam.release()
+		        cv2.destroyAllWindows()
+        		break
+        	img = cv2.add(frame,mask)
+
+        	cv2.putText(img, "Press ESC to close.", (5, 25),
                     cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255,255,255))
-        cv2.imshow("Raspberry Potter", img)
+        	cv2.imshow("Raspberry Potter", frame)
 
-        # get next frame
-        rval, frame = cam.read()
+        	# get next frame
+        	rval, frame = cam.read()
 
-        # Now update the previous frame and previous points
-        old_gray = frame_gray.copy()
-        p0 = good_new.reshape(-1,1,2)
-        key = cv2.waitKey(20)
-        if key in [27, ord('Q'), ord('q')]: # exit on ESC
-            break
+        	# Now update the previous frame and previous points
+        	old_gray = frame_gray.copy()
+        	p0 = good_new.reshape(-1,1,2)
+        	key = cv2.waitKey(20)
+        	if key in [27, ord('Q'), ord('q')]: # exit on ESC
+            		break
             
 FindWand()
 TrackWand()            
 cv2.destroyAllWindows()
 cam.release()  
-
